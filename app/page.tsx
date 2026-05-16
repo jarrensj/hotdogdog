03e6724afe8d259262df6cb249e98c940905e0b2
Jarren San Jose <sanjose.jarren@gmail.com>
2026-05-15 21:24:18 -0700

    feat: add disabled 'coming soon' button below the hotdog

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 flex-col items-center justify-center gap-6">
       <Image
         src="/hotdogdog.png"
         alt="hot dog dog"
@@ -10,6 +10,12 @@ export default function Home() {
         height={200}
         priority
       />
+      <button
+        disabled
+        className="rounded-full border border-black/20 px-6 py-2 text-sm font-medium text-black/50 cursor-not-allowed"
+      >
+        coming soon
+      </button>
     </div>
   );
 }
