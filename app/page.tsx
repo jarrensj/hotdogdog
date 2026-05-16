fe2ead4c5d0d10b46f7aebafa2423d8b679c5537
Jarren San Jose <sanjose.jarren@gmail.com>
2026-05-15 21:22:30 -0700

    feat: add image of the hotdogdog

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex flex-1 items-center justify-center">
-      <h1>hot dog dog</h1>
+      <Image
+        src="/hotdogdog.png"
+        alt="hot dog dog"
+        width={200}
+        height={200}
+        priority
+      />
     </div>
   );
 }
